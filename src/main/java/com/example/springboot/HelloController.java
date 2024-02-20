cccf114ad6f123ecb03560f56667e2e94a68605e
xiaoxin su <sxx1203@gmail.com>
2024-02-20 15:27:43 -0600

    correct bug

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -1,5 +1,7 @@
 package com.example.springboot;
 
+import java.util.Date;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +10,7 @@ public class HelloController {
 
 	@GetMapping("/")
 	public String index() {
-		return "Hello Oliver and Sarah Today is " + new Date() + "!";
+		return "Hello Oliver and Sarah, right now the time is " + new Date() + "!";
 	}
 
 }
